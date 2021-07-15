8df7f6a1d2dd23eddf0c8d54bb3c7a6565024db2
Your Name <you@example.com>
2021-07-15 16:28:23 -0400

    fix: updated settings to match previous environment

--- scripts/globals/settings.lua
+++ scripts/globals/settings.lua
@@ -22,25 +22,25 @@ xi.settings =
     ENABLE_COP       = 1,
     ENABLE_TOAU      = 1,
     ENABLE_WOTG      = 1,
-    ENABLE_ACP       = 1,
-    ENABLE_AMK       = 1,
-    ENABLE_ASA       = 1,
-    ENABLE_ABYSSEA   = 1,
-    ENABLE_SOA       = 1,
-    ENABLE_ROV       = 1,
-    ENABLE_VOIDWATCH = 1, -- Not an expansion, but has its own storyline. (Not Implemented)
+    ENABLE_ACP       = 0,
+    ENABLE_AMK       = 0,
+    ENABLE_ASA       = 0,
+    ENABLE_ABYSSEA   = 0,
+    ENABLE_SOA       = 0,
+    ENABLE_ROV       = 0,
+    ENABLE_VOIDWATCH = 0, -- Not an expansion, but has its own storyline. (Not Implemented)
 
     -- FIELDS OF VALOR/Grounds of Valor settings
-    ENABLE_FIELD_MANUALS  = 1, -- Enables Fields of Valor
-    ENABLE_GROUNDS_TOMES  = 1, -- Enables Grounds of Valor
+    ENABLE_FIELD_MANUALS  = 0, -- Enables Fields of Valor
+    ENABLE_GROUNDS_TOMES  = 0, -- Enables Grounds of Valor
     ENABLE_SURVIVAL_GUIDE = 1, -- Enables Survival Guides (Not Implemented)
     REGIME_WAIT           = 1, -- Make people wait till 00:00 game time as in retail. If it's 0, there is no wait time.
     FOV_REWARD_ALLIANCE   = 0, -- Allow Fields of Valor rewards while being a member of an alliance. (default retail behavior: 0)
     GOV_REWARD_ALLIANCE   = 1, -- Allow Grounds of Valor rewards while being a member of an alliance. (default retail behavior: 1)
 
     -- Records of Eminence
-    ENABLE_ROE            = 1,
-    ENABLE_ROE_TIMED      = 1, -- Enable 4-hour timed records
+    ENABLE_ROE            = 0,
+    ENABLE_ROE_TIMED      = 0, -- Enable 4-hour timed records
     ENABLE_EXCHANGE_LIMIT = 1, -- Enable Maximum limit of sparks spent per Week (default retail behavior: 1)
 
     WEEKLY_EXCHANGE_LIMIT = 100000, -- Maximum amount of sparks/accolades that can be spent per week (default retail value: 100000)
@@ -52,40 +52,40 @@ xi.settings =
     CAP_CURRENCY_VALOR     = 50000,
 
     -- Magian Trials
-    ENABLE_MAGIAN_TRIALS             = 1,
-    MAGIAN_TRIALS_MOBKILL_MULTIPLIER = 1,
-    MAGIAN_TRIALS_TRADE_MULTIPLIER   = 1,
+    ENABLE_MAGIAN_TRIALS             = 0,
+    MAGIAN_TRIALS_MOBKILL_MULTIPLIER = 0,
+    MAGIAN_TRIALS_TRADE_MULTIPLIER   = 0,
 
     -- VoidWalker
-    ENABLE_VOIDWALKER = 1,
+    ENABLE_VOIDWALKER = 0,
 
     -- TREASURE CASKETS
     -- Retail droprate = 0.1 (10%) with no other effects active
     -- Set to 0 to disable caskets.
     -- max is clamped to 1.0 (100%)
-    CASKET_DROP_RATE = 0.1,
+    CASKET_DROP_RATE = 0.0,
 
     -- Setting to lock content more accurately to the content you have defined above
     -- This generally results in a more accurate presentation of your selected expansions
     -- as well as a less confusing player experience for things that are disabled (things that are disabled are not loaded)
     -- This feature correlates to the content_tag column in the SQL files
-    RESTRICT_CONTENT = 0,
+    RESTRICT_CONTENT = 1,
 
     -- CHARACTER CONFIG
     INITIAL_LEVEL_CAP              = 50, -- The initial level cap for new players.  There seems to be a hardcap of 255.
-    MAX_LEVEL                      = 99, -- Level max of the server, lowers the attainable cap by disabling Limit Break quests.
+    MAX_LEVEL                      = 75, -- Level max of the server, lowers the attainable cap by disabling Limit Break quests.
     NORMAL_MOB_MAX_LEVEL_RANGE_MIN = 0,  -- Lower Bound of Max Level Range for Normal Mobs (0 = Uncapped)
     NORMAL_MOB_MAX_LEVEL_RANGE_MAX = 0,  -- Upper Bound of Max Level Range for Normal Mobs (0 = Uncapped)
     START_GIL                      = 10, -- Amount of gil given to newly created characters.
-    START_INVENTORY                = 30, -- Starting inventory and satchel size.  Ignores values < 30.  Do not set above 80!
-    NEW_CHARACTER_CUTSCENE         = 1,  -- Set to 1 to enable opening cutscenes, 0 to disable.
+    START_INVENTORY                = 80, -- Starting inventory and satchel size.  Ignores values < 30.  Do not set above 80!
+    NEW_CHARACTER_CUTSCENE         = 0,  -- Set to 1 to enable opening cutscenes, 0 to disable.
     SUBJOB_QUEST_LEVEL             = 18, -- Minimum level to accept either subjob quest.  Set to 0 to start the game with subjobs unlocked.
     ADVANCED_JOB_LEVEL             = 30, -- Minimum level to accept advanced job quests.  Set to 0 to start the game with advanced jobs.
-    ALL_MAPS                       = 0,  -- Set to 1 to give starting characters all the maps.
-    UNLOCK_OUTPOST_WARPS           = 0,  -- Set to 1 to give starting characters all outpost warps.  2 to add Tu'Lia and Tavnazia.
+    ALL_MAPS                       = 1,  -- Set to 1 to give starting characters all the maps.
+    UNLOCK_OUTPOST_WARPS           = 2,  -- Set to 1 to give starting characters all outpost warps.  2 to add Tu'Lia and Tavnazia.
 
     SHOP_PRICE      = 1.000, -- Multiplies prices in NPC shops.
-    GIL_RATE        = 1.000, -- Multiplies gil earned from quests.  Won't always display in game.
+    GIL_RATE        = 10.000, -- Multiplies gil earned from quests.  Won't always display in game.
     BAYLD_RATE      = 1.000, -- Multiples bayld earned from quests.
     EXP_RATE        = 1.000, -- Multiplies exp earned from fov and quests.
     TABS_RATE       = 1.000, -- Multiplies tabs earned from fov.
@@ -99,24 +99,24 @@ xi.settings =
     DARK_POWER      = 1.000, -- Multiplies amount drained by Dark Magic.
     ITEM_POWER      = 1.000, -- Multiplies the effect of items such as Potions and Ethers.
     WEAPON_SKILL_POWER  = 1.000, -- Multiplies damage dealt by Weapon Skills.
-    WEAPON_SKILL_POINTS = 1.000, -- Multiplies points earned during weapon unlocking. (Not Implemented)
+    WEAPON_SKILL_POINTS = 25.000, -- Multiplies points earned during weapon unlocking. (Not Implemented)
     USE_ADOULIN_WEAPON_SKILL_CHANGES = true, -- true/false. Change to toggle new Adoulin weapon skill damage calculations
 
     -- TRUSTS
-    ENABLE_TRUST_CASTING = 1,
-    ENABLE_TRUST_QUESTS  = 1,
+    ENABLE_TRUST_CASTING = 0,
+    ENABLE_TRUST_QUESTS  = 0,
 
-    HARVESTING_BREAK_CHANCE = 33, -- % chance for the sickle to break during harvesting.  Set between 0 and 100.
-    EXCAVATION_BREAK_CHANCE = 33, -- % chance for the pickaxe to break during excavation.  Set between 0 and 100.
-    LOGGING_BREAK_CHANCE    = 33, -- % chance for the hatchet to break during logging.  Set between 0 and 100.
-    MINING_BREAK_CHANCE     = 33, -- % chance for the pickaxe to break during mining.  Set between 0 and 100.
-    HARVESTING_RATE         = 50, -- % chance to recieve an item from haresting.  Set between 0 and 100.
-    EXCAVATION_RATE         = 50, -- % chance to recieve an item from excavation.  Set between 0 and 100.
-    LOGGING_RATE            = 50, -- % chance to recieve an item from logging.  Set between 0 and 100.
-    MINING_RATE             = 50, -- % chance to recieve an item from mining.  Set between 0 and 100.
-    DIGGING_RATE            = 85, -- % chance to receive an item from chocbo digging during favorable weather.  Set between 0 and 100.
+    HARVESTING_BREAK_CHANCE = 0, -- % chance for the sickle to break during harvesting.  Set between 0 and 100.
+    EXCAVATION_BREAK_CHANCE = 0, -- % chance for the pickaxe to break during excavation.  Set between 0 and 100.
+    LOGGING_BREAK_CHANCE    = 0, -- % chance for the hatchet to break during logging.  Set between 0 and 100.
+    MINING_BREAK_CHANCE     = 0, -- % chance for the pickaxe to break during mining.  Set between 0 and 100.
+    HARVESTING_RATE         = 100, -- % chance to recieve an item from haresting.  Set between 0 and 100.
+    EXCAVATION_RATE         = 100, -- % chance to recieve an item from excavation.  Set between 0 and 100.
+    LOGGING_RATE            = 100, -- % chance to recieve an item from logging.  Set between 0 and 100.
+    MINING_RATE             = 100, -- % chance to recieve an item from mining.  Set between 0 and 100.
+    DIGGING_RATE            = 100, -- % chance to receive an item from chocbo digging during favorable weather.  Set between 0 and 100.
 
-    HEALING_TP_CHANGE       = -100, -- Change in TP for each healing tick. Default is -100
+    HEALING_TP_CHANGE       = 0, -- Change in TP for each healing tick. Default is -100
 
     -- SE implemented coffer/chest illusion time in order to prevent coffer farming. No-one in the same area can open a chest or coffer for loot (gil, gems & items)
     -- till a random time between MIN_ILLSION_TIME and MAX_ILLUSION_TIME. During this time players can loot keyitem and item related to quests (AF, maps... etc.)
@@ -127,8 +127,8 @@ xi.settings =
 
     -- Sets spawn type for: Behemoth, Fafnir, Adamantoise, King Behemoth, Nidhog, Aspidochelone.
     -- Use 0 for timed spawns, 1 for force pop only, 2 for both
-    LandKingSystem_NQ = 1,
-    LandKingSystem_HQ = 1,
+    LandKingSystem_NQ = 2,
+    LandKingSystem_HQ = 2,
 
     -- Multiplier to NM lottery spawn chance. (Default 1.0) eg. 0 = disable lottery spawns. -1 for always 100% chance.
     NM_LOTTERY_CHANCE = 1.0,
@@ -150,8 +150,8 @@ xi.settings =
     AF1_QUEST_LEVEL = 40,    -- Minimum level to start AF1 quest
     AF2_QUEST_LEVEL = 50,    -- Minimum level to start AF2 quest
     AF3_QUEST_LEVEL = 50,    -- Minimum level to start AF3 quest
-    OLDSCHOOL_G1    = false, -- Set to true to require farming Exoray Mold, Bombd Coal, and Ancient Papyrus drops instead of allowing key item method.
-    OLDSCHOOL_G2    = false, -- Set true to require the NMs for "Atop the Highest Mountains" be dead to get KI like before SE changed it.
+    OLDSCHOOL_G1    = true, -- Set to true to require farming Exoray Mold, Bombd Coal, and Ancient Papyrus drops instead of allowing key item method.
+    OLDSCHOOL_G2    = true, -- Set true to require the NMs for "Atop the Highest Mountains" be dead to get KI like before SE changed it.
     FRIGICITE_TIME  = 30,    -- When OLDSCHOOL_G2 is enabled, this is the time (in seconds) you have from killing Boreal NMs to click the "???" target.
 
     -- SPELL SPECIFIC SETTINGS
@@ -174,19 +174,28 @@ xi.settings =
     HALLOWEEN_YEAR_ROUND            = 0,  -- Set to 1 to have Harvest Festival initialize outside of normal times.
 
     -- MISC
-    HOMEPOINT_HEAL               = 0,    -- Set to 1 if you want Home Points to heal you like in single-player Final Fantasy games.
+    HOMEPOINT_HEAL               = 1,    -- Set to 1 if you want Home Points to heal you like in single-player Final Fantasy games.
     RIVERNE_PORTERS              = 120,  -- Time in seconds that Unstable Displacements in Cape Riverne stay open after trading a scale.
     LANTERNS_STAY_LIT            = 1200, -- time in seconds that lanterns in the Den of Rancor stay lit.
     ENABLE_COP_ZONE_CAP          = 0,    -- enable or disable lvl cap
     ALLOW_MULTIPLE_EXP_RINGS     = 0,    -- Set to 1 to remove ownership restrictions on the Chariot/Empress/Emperor Band trio.
-    BYPASS_EXP_RING_ONE_PER_WEEK = 0,    -- -- Set to 1 to bypass the limit of one ring per Conquest Tally Week.
+    BYPASS_EXP_RING_ONE_PER_WEEK = 1,    -- -- Set to 1 to bypass the limit of one ring per Conquest Tally Week.
     NUMBER_OF_DM_EARRINGS        = 1,    -- Number of earrings players can simultaneously own from Divine Might before scripts start blocking them (Default: 1)
     HOMEPOINT_TELEPORT           = 1,    -- Enables the homepoint teleport system
-    DIG_ABUNDANCE_BONUS          = 0,    -- Increase chance of digging up an item (450  = item digup chance +45)
-    DIG_FATIGUE                  = 1,    -- Set to 0 to disable Dig Fatigue
-    DIG_GRANT_BURROW             = 0,    -- Set to 1 to grant burrow ability
-    DIG_GRANT_BORE               = 0,    -- Set to 1 to grant bore ability
+    DIG_ABUNDANCE_BONUS          = 300,    -- Increase chance of digging up an item (450  = item digup chance +45)
+    DIG_FATIGUE                  = 0,    -- Set to 0 to disable Dig Fatigue
+    DIG_GRANT_BURROW             = 1,    -- Set to 1 to grant burrow ability
+    DIG_GRANT_BORE               = 1,    -- Set to 1 to grant bore ability
     ENM_COOLDOWN                 = 120,  -- Number of hours before a player can obtain same KI for ENMs (default: 5 days)
     FORCE_SPAWN_QM_RESET_TIME    = 300,  -- Number of seconds the ??? remains hidden for after the despawning of the mob it force spawns.
     GOBBIE_BOX_MIN_AGE           = 45,   -- Minimum character age in days before a character can sign up for Gobbie Mystery Box
+
+    --METAL CHANGES
+    DISABLE_LOTTERY_NM_SYSTEM            = 1, -- Set to 1 to disable the lottery system for NMs and have them spawn like normal mobs.
+    ALWAYS_GRANT_NATION_RING             = 1, -- Set to 1 to always grant a nation ring regardless of race.
+    STARTING_WARP_RING                   = 1, -- Set to 1 to grant a warp ring at character creation.
+    ALWAYS_ALLOW_CHOCOBO_RENTAL          = 0, -- Set to 1 to always allow a chocobo rental without a license or any level.
+    FREE_HOMEPOINT_TELEPORTS             = 1, -- Set to 1 to make Homepoint Teleporting free.
+    DISABLE_DROPTYPE_ONE                 = 1, -- Disables "grouped loot" tpye, giving each item a chance to drop in a group
+    IGNORE_NATION_FOR_CONQUEST_PURCHASE  = 1, -- When purchasing from Conquest Items from Nation Guard will display all items regardless of Nation or conquest rank
 }
