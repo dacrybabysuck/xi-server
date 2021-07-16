190ba1327fb46f6931e73d5d6f473ae5cce487c8
Your Name <you@example.com>
2021-07-16 16:36:40 -0400

    fix: update roc and simurgh after migrating to landsandboat

--- scripts/zones/Sauromugue_Champaign/mobs/Roc.lua
+++ scripts/zones/Sauromugue_Champaign/mobs/Roc.lua
@@ -2,10 +2,27 @@
 -- Area: Sauromugue Champaign (120)
 --  HNM: Roc
 -----------------------------------
-mixins = {require("scripts/mixins/rage")}
+mixins =
+{
+    require("scripts/mixins/job_special"),
+    require("scripts/mixins/rage")
+}
 require("scripts/globals/titles")
+require("scripts/globals/status")
 -----------------------------------
 local entity = {}
+
+entity.onMobInitialize = function(mob)
+    mob:setMobMod(xi.mobMod.DRAW_IN, 1)
+end
+
+entity.onMobSpawn = function(mob)
+    xi.mix.jobSpecial.config(mob, {
+        specials = {
+            {id = xi.jsa.BENEDICTION, hpp = 10},
+        },
+    })
+end
 
 entity.onMobDeath = function(mob, player, isKiller)
     player:addTitle(xi.title.ROC_STAR)
